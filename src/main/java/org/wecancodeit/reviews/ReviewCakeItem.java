615a9294a92e265a017ac755261642eb6d26d6cc
basheeraa <basheera_a@hotmail.com>
2021-01-09 09:46:41 -0500

    test

--- src/main/java/org/wecancodeit/reviews/ReviewCakeItem.java
+++ src/main/java/org/wecancodeit/reviews/ReviewCakeItem.java
@@ -8,15 +8,16 @@ public class ReviewCakeItem {
     private String shape;
     private String description;
 
+    private long id;
 
-
-    public ReviewCakeItem(CakeType type, String flavor, String icing, double price, String shape, String description) {
+    public ReviewCakeItem(CakeType type, String flavor, String icing, double price, String shape, String description, long id) {
         this.type = type;
         this.flavor = flavor;
         this.icing = icing;
         this.price = price;
         this.shape = shape;
         this.description = description;
+        this.id = id;
     }
 
     public CakeType getType() {
@@ -40,4 +41,8 @@ public class ReviewCakeItem {
     }
 
     public String getDescription() { return description;}
+
+    public long getId() {
+        return id;
+    }
 }
--- src/main/java/org/wecancodeit/reviews/ReviewCakeItemController.java
+++ src/main/java/org/wecancodeit/reviews/ReviewCakeItemController.java
@@ -1,6 +1,7 @@
 package org.wecancodeit.reviews;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collection;
@@ -10,28 +11,32 @@ import java.util.Map;
 
 @Controller
 public class ReviewCakeItemController {
-    Map<String, ReviewCakeItem> reviewCakeItemMap = new HashMap<>();
+    Map<Long, ReviewCakeItem> reviewCakeItemMap = new HashMap<>();
     public ReviewCakeItemController() {
         CakeType sampleCakeType = new CakeType("wedding cake", Collections.EMPTY_LIST, 1L);
-        ReviewCakeItem sampleCakeItem = new ReviewCakeItem(sampleCakeType, "chocolate", "cream cheese", 20.00, "round", "good");
-        reviewCakeItemMap.put("wedding cake", sampleCakeItem);
+        ReviewCakeItem sampleCakeItem = new ReviewCakeItem(sampleCakeType, "chocolate", "cream cheese", 20.00, "round", "good", 1L);
+        reviewCakeItemMap.put(1L, sampleCakeItem);
 
     }
 
-    @RequestMapping("sample-cake-item")
-    public String getCakeItem(Model model){
-        CakeType sampleCakeType = new CakeType("wedding cake", Collections.EMPTY_LIST, 1L);
-        ReviewCakeItem sampleCakeItem = new ReviewCakeItem(sampleCakeType, "chocolate", "cream cheese", 20.00, "round", "good");
-        model.addAttribute("ReviewCakeItem", sampleCakeItem);
+    @RequestMapping("sample-cake-item/{id}")
+    public String getCakeItem(Model model, @PathVariable long id) {-
+        model.addAttribute("ReviewCakeItem", reviewCakeItemMap.get(id));
         return "review-cake-item-template";
 
+    }
 
 
 
 
 
 
-            }
+
+
+
+
+
+
 
 
 
